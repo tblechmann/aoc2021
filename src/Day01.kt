a995459513ce740ebf840d503d767d3ea84a6021
tobias.blechmann <tobias.blechmann@dumont.de>
2021-12-10 20:33:37 +0100

    day 1 **

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,10 +13,28 @@ fun main() {
         return counter
     }
 
+    fun part2(input: List<String>): Int {
+        var cmp = Integer.MAX_VALUE
+        var counter = 0
+
+        val intList = ArrayList<Int>(input.map(Integer::parseInt))
+        val resultList = ArrayList<String>()
+
+        while (intList.size >= 3) {
+                    val sum = intList.removeAt(0) + intList.get(0) + intList.get(1)
+                    resultList.add(sum.toString())
+        }
+
+        return part1(resultList)
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
+    println(part2(input))
+
 }
