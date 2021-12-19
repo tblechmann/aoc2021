b986d207a59fe36a58e0e19f37ac35d00cd81a33
tobias.blechmann <tobias.blechmann@dumont.de>
2021-12-19 12:45:08 +0100

    day 10 **

--- src/Day10.kt
+++ src/Day10.kt
@@ -5,22 +5,25 @@ fun main() {
         return input.fold(0) { acc, line -> acc+SyntaxLine(line).getSyntaxErrorValue() }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): Long {
+        return input.map { SyntaxLine(it) }
+            .filter { it.errorChar == null }
+            .map {  it.getAutoCompleteScore()}
+            .sorted()
+            .let { it.get(it.size/2) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
     check(part1(testInput) == 26397)
-    //check(part2(testInput) == 5)
+    check(part2(testInput) == 288957L)
 
     val input = readInput("Day10")
     println(part1(input))
-    //println(part2(input))
-
+    println(part2(input))
 }
 
-class SyntaxLine(val line: String) {
+class SyntaxLine(line: String) {
 
     val stack = Stack<Char>()
     var errorChar: Char? = null
@@ -33,6 +36,12 @@ class SyntaxLine(val line: String) {
     Pair('}', 1197),
     Pair('<', 25137),
     Pair('>', 25137))
+
+    val autoCompletionValues = mapOf(
+        Pair('(', 1),
+        Pair('[', 2),
+        Pair('{', 3),
+        Pair('<', 4))
 
     init {
          for(c in line.toCharArray()) {
@@ -50,6 +59,11 @@ class SyntaxLine(val line: String) {
     fun getSyntaxErrorValue(): Int {
         return if (errorChar != null) errorValues[errorChar]!! else 0
     }
+
+    fun getAutoCompleteScore(): Long {
+        val score = stack.toArray().reversed().fold(0L) { acc, value -> (acc*5) + autoCompletionValues[value]!! }
+        return score
+    }
 }
 
 fun Char.isOpeningBracket(): Boolean {
@@ -58,16 +72,6 @@ fun Char.isOpeningBracket(): Boolean {
         '[' -> true
         '{' -> true
         '<' -> true
-        else -> false
-    }
-}
-
-fun Char.matchesClosingBracket(closingBracket: Char): Boolean {
-    return when(this) {
-        '(' -> closingBracket == ')'
-        '[' -> closingBracket == ']'
-        '{' -> closingBracket == '}'
-        '<' -> closingBracket == '>'
         else -> false
     }
 }
